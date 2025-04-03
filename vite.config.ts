0807c902642803fcf6bfa40bfbb3cfbd1fc92c08
Drew Showalter <10665739+one19@users.noreply.github.com>
2025-04-04 03:04:53 +1100

    🔧 support latex math equations.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import mdx from '@mdx-js/rollup';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/the-css-animation-sweeping-the-nation/', // CHANGE WHEN CLONING
   plugins: [
-    { enforce: 'pre', ...mdx({ providerImportSource: '@mdx-js/react' }) },
+    {
+      enforce: 'pre',
+      ...mdx({
+        providerImportSource: '@mdx-js/react',
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
+      }),
+    },
     react(),
   ],
 });
